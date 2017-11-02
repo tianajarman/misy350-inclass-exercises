78976a60abed31392d30b5811f9666e30ee887d3
Tiana Jarman <tjarman@udel.edu>
2017-11-02 13:18:55 -0400

    bs added

---
+++ 11-2-2017/bs-basics.js
@@ -0,0 +1,10 @@
+
+function main () {
+$('#success-button').click(function () {
+  $('#exampleModal').modal('show');
+
+});
+}
+
+
+$(document).ready(main); //absolutely needed to run the javascript main function above!!!!
